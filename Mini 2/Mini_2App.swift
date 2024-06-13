0f5192743fd16d705cebf4becec94344e3e8d9a7
zefanyaarifin15 <137996079+zefanyaarifin15@users.noreply.github.com>
2024-06-13 14:52:29 +0700

    Initial Commit

---
+++ Mini 2/Mini_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_2App.swift
+//  Mini 2
+//
+//  Created by Zefanya on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
