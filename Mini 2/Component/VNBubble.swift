23d97e39e0599160422bcddff5c93477792d092a
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-06-16 16:54:12 +0700

    sound model, sound view model, vn bubble component done

--- Mini 2/Component/VNBubble.swift
+++ Mini 2/Component/VNBubble.swift
@@ -1,11 +1,46 @@
 import SwiftUI
 
 struct VNBubble: View {
+    @ObservedObject var viewModel: SoundViewModel
+    let sound: Sound
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    
+                    
+                    Button(action: {
+                        viewModel.playSound(named: sound.filename)
+                    }) {
+                        Image(systemName: "play.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(.black)
+                    }
+                    .frame(width: 40, height: 50)
+                    
+                    Image("SoundWave")
+                        .resizable()
+                        .frame(width: 200, height: 150)
+                        .padding(.trailing, 1)
+                }
+                .padding(.leading, 5)
+            }
+            .frame(width: 250, height: 30)
+            .padding()
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(20)
+            .shadow(radius: 2)
+            
+            Spacer()
+        }
+        .padding()
     }
 }
 
 #Preview {
-    VNBubble()
+    let viewModel = SoundViewModel()
+    let sampleSound = Sound(id: "0", title: "Sound1", filename: "Sound1")
+    return VNBubble(viewModel: viewModel, sound: sampleSound)
 }
+
