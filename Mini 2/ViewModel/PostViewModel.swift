5b9773048ba7d42e8650f49244148089d7fcc22f
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-06-23 23:50:58 +0700

    notif n call ui

---
+++ Mini 2/ViewModel/PostViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PostViewModel.swift
+//  Mini 2
+//
+//  Created by Jasmine Mutia Alifa on 23/06/24.
+//
+
+import Foundation
+
+let posts: [Post] = [
+    Post(id: 1, username: "beautyjasmine", caption: "Nice photoshoot🫶🏻", location: "Studio", postImage: "post1", profileImage: "beautyjasmine", initialLikes: 811234, commentUser: "ShadowLurker", commentText: "That lips look like pongo lips"),
+    Post(id: 2, username: "beautyjasmine", caption: "Great experience🫶🏻", location: "Studio", postImage: "post2", profileImage: "beautyjasmine", initialLikes: 214321, commentUser: "ShadowLurker", commentText: "Are you always at the studio?"),
+    Post(id: 3, username: "beautyjasmine", caption: "Favorite pose🫶🏻", location: "Cafe", postImage: "post3", profileImage: "beautyjasmine", initialLikes: 246789, commentUser: "ShadowLurker", commentText: "That pose again? not creative LOL"),
+    Post(id: 4, username: "beautyjasmine", caption: "Lovely day🫶🏻", location: "Cafe", postImage: "post4", profileImage: "beautyjasmine", initialLikes: 106789, commentUser: "ShadowLurker", commentText: "Your outfit looks ugly."),
+    Post(id: 5, username: "beautyjasmine", caption: "Eiffel Tower🌸", location: "Paris", postImage: "post5", profileImage: "beautyjasmine", initialLikes: 398567, commentUser: "ShadowLurker", commentText: "I bet you don’t even like the view."),
+    Post(id: 6, username: "beautyjasmine", caption: "Favorite pose🫶🏻", location: "Cafe", postImage: "post6", profileImage: "beautyjasmine", initialLikes: 785634, commentUser: "ShadowLurker", commentText: "That pose again? not creative LOL")
+    // Add more posts as needed
+]
