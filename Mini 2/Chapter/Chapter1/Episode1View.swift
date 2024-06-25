1cc378e192502c87a2d8dc20ac4523a9e5fe20ae
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-06-25 15:42:36 +0700

    chapter1 bikin episode1View

--- Mini 2/Chapter/Chapter1/Episode1View.swift
+++ Mini 2/Chapter/Chapter1/Episode1View.swift
@@ -15,15 +15,12 @@ struct Episode1View: View {
                     PhoneView().overlay{
                         VStack{
                             NavigationLink(destination: DMsView(viewModel: DialogViewModel(), viewModel_2: DMsViewModel())) {
-                                                        MessageNotification(image: "icon", title: "Stephanie", description: "Hi, how are you?", time: "19:00")
+                                                        MessageNotification(image: "icon", title: "Stephanie", description: "You got 1K+ comments on your latest post", time: "19:00")
                                                     }
                             Spacer()
                         }
                     }
-                    
-                 // Memanggil PhoneView untuk menampilkan layar
-                   
-//                    Spacer()
+                
                     
                 }
             }
