45ddc0882af962d627d537cd9db370245fb972d4
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-06-26 12:22:19 +0700

    add eps 7

--- Mini 2/Chapter/Episode7View.swift
+++ Mini 2/Chapter/Episode7View.swift
@@ -8,11 +8,83 @@
 import SwiftUI
 
 struct Episode7View: View {
+    
+    @ObservedObject var viewModel_2: DMsViewModel
+    
+    @State private var showChatView = false
+    @State private var showSecondNotification = false
+    
+    let targetPost: Post = Post(
+        username: "ShadowLurker",
+        caption: "Only idiots use this crappy products",
+        location: "Science Lab",
+        postImage: "feeds1",
+        profileImage: "Stalker",
+        initialLikes: 123,
+        commentUser: "user123",
+        commentText: "No beauty influencer to be trusted.."
+    )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                VStack {
+                    PhoneView().overlay {
+                        VStack {
+                            if !showChatView {
+                                Button(action: {
+                                    showChatView = true
+                                    showSecondNotification = true // Show second notification when navigating to chat
+                                }) {
+                                    MessageNotification(
+                                        image: "icon",
+                                        title: "Fans",
+                                        description: "Hi, have you seen this post?",
+                                        time: "now",
+                                        disableSecondNotification: true
+                                    )
+                                }
+                                .sheet(isPresented: $showChatView) {
+                                    ChatView(viewModel: DialogViewModel(), partner: "Fans")
+                                }
+                            } else {
+                                if showSecondNotification {
+                                    NavigationLink(
+                                        destination: PostView(
+                                            username: targetPost.username,
+                                            caption: targetPost.caption,
+                                            location: targetPost.location,
+                                            postImage: targetPost.postImage,
+                                            profileImage: targetPost.profileImage,
+                                            initialLikes: targetPost.initialLikes,
+                                            commentUser: targetPost.commentUser,
+                                            commentText: targetPost.commentText
+                                        )
+                                    ) {
+                                        MessageNotification(
+                                            image: "icon",
+                                            title: "InstaQueen",
+                                            description: "ShadowLurker posted something new",
+                                            time: "now",
+                                            disableSecondNotification: true
+                                        )
+                                    }
+                                }
+                            }
+                            Spacer()
+                        }
+                    }
+                }
+            }
+        }
+        .onAppear {
+            viewModel_2.addDM(.natalie)
+        }
     }
 }
 
+
+
 #Preview {
-    Episode7View()
+    Episode7View(viewModel_2: DMsViewModel())
 }
