b8adb6b8ff079ade43654b211d6f8fb08901e904
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-06-26 12:21:54 +0700

    add eps 7

---
+++ Mini 2/Chapter/Episode7View.swift
@@ -0,0 +1,18 @@
+//
+//  Episode7View.swift
+//  Mini 2
+//
+//  Created by Natasha Radika on 26/06/24.
+//
+
+import SwiftUI
+
+struct Episode7View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Episode7View()
+}
