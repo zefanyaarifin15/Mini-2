b88ac4e5e7a9ea77e6a8e51eea3016be5fc99248
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-06-26 11:23:40 +0700

    masukin jason message buat eps 4

--- Mini 2/Chapter/Episode4View.swift
+++ Mini 2/Chapter/Episode4View.swift
@@ -1,41 +1,41 @@
 import SwiftUI
 
 struct Episode4View: View {
-    let targetPost: Post = Post(
-        username: "beautyjasmine",
-        caption: "My niece is so cute",
-        location: "Home",
-        postImage: "feeds1",
-        profileImage: "beautyjasmine",
-        initialLikes: 319872,
-        commentUser: "ShadowLurker",
-        commentText: "You're nothing but a fake"
-    )
+    @State private var navigateToChat = false
     
     var body: some View {
-        NavigationView{
-            ZStack{
-                VStack{
-                    PhoneView().overlay{
-                        VStack{
-                            MessageNotification(
-                                image: "icon",
-                                title: "Rose",
-                                description: "I’m worried about you, are you alright? I’m here for you all the time.",
-                                time: "19:00",
-                                disableSecondNotification: true
-                            )
-                            Spacer()
-                        }
-                        //ntr abis di klik notif nya munculin chat dm dia sama rose
-                        
-                        //logout dari ig jasmine di hp user (belum bikin UI seolah dia nge logout dari hp jasmine)
-                    }
+        VStack {
+            PhoneView().overlay {
+                VStack {
+                    MessageNotification(
+                        image: "icon",
+                        title: "Rose",
+                        description: "I’m worried about you, are you alright? I’m here for you all the time.",
+                        time: "19:00",
+                        disableSecondNotification: true
+                    )
+                    Spacer()
                 }
             }
+            
+            //ini bingung knp dia navigate ke post view?
+            
+            .background(
+                NavigationLink(destination: ChatView(viewModel: DialogViewModel(), partner: "Rose2"), isActive: $navigateToChat) {
+                    EmptyView()
+                }
+                .hidden()
+            )
+            .onTapGesture {
+                navigateToChat = true
+            }
+            
+            //abis itu dia lgsg logout ig dari hp rose
+            
         }
     }
 }
+
 
 struct Episode4View_Previews: PreviewProvider {
     static var previews: some View {
