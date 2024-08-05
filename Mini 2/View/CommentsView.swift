70ced5a5ec5695e3e5164e6dc426abc99df66898
Jasmine <jasmine.mutia03@gmail.com>
2024-08-05 18:27:37 +0700

    comment biar sesuai di view all comment dan comment jadi paling atas di view all comments

--- Mini 2/View/CommentsView.swift
+++ Mini 2/View/CommentsView.swift
@@ -1,39 +1,110 @@
-//
+
 //  CommentsView.swift
 //  Mini 2
 //
 //  Created by Jasmine Mutia Alifa on 25/06/24.
+
+
+//import SwiftUI
+//
+//struct CommentsView: View {
+//    @EnvironmentObject private var progress: UserProgress
+//    @Binding var commentUser: String
+//    @Binding var commentText: String
+//    
+//    var comments: [Comment] {
+//        var tempComments = [
+//            Comment(username: "makeupfanatic", text: "Love this look! Can you share your routine?"),
+//            Comment(username: "beautyjunkie", text: "Your makeup is always on point!"),
+//            Comment(username: "ShadowLurker", text: "You're nothing but a fake"),
+//            Comment(username: "beautylover", text: "Absolutely stunning!"),
+//            Comment(username: "glamqueen", text: "You look gorgeous! What's your lipstick shade?"),
+//            Comment(username: "nataliebert", text: "So stunning😍"),
+//            Comment(username: "stephaniejenn", text: "Love this look on you!❤️"),
+//            Comment(username: "rosemisela", text: "Absolutely beautiful!"),
+//            Comment(username: "makeupaddict", text: "I need this look in my life!"),
+//            Comment(username: "beautybabe", text: "Your makeup is flawless!"),
+//            Comment(username: "cosmeticqueen", text: "You're glowing!"),
+//            Comment(username: "makeuplover", text: "Stunning as always!😍"),
+//            Comment(username: "makeupenthusiast", text: "Can't get over how amazing you look!"),
+//            Comment(username: "beautyobsessed", text: "You're my makeup inspiration!"),
+//            Comment(username: "glamourfan", text: "Such a beautiful look!"),
+//            Comment(username: "makeupgeek", text: "Love this so much!"),
+//            Comment(username: "beautyaddicted", text: "Your makeup skills are incredible!"),
+//            Comment(username: "cosmeticfanatic", text: "You always look amazing!"),
+//            Comment(username: "glamsquad", text: "Love this! You're so talented!"),
+//            Comment(username: "beautyicon", text: "Absolutely gorgeous!")
+//        ]
+//        
+//        if let index = tempComments.firstIndex(where: { $0.username == "ShadowLurker" }) {
+//            tempComments[index] = Comment(username: commentUser, text: commentText)
+//        }
+//        
+//        return tempComments
+//    }
+//    
+//    var body: some View {
+//        List(comments) { comment in
+//            HStack {
+//                Text(comment.username)
+//                    .font(.system(size: 16))
+//                    .bold()
+//                
+//                Text(comment.text)
+//                    .font(.system(size: 16))
+//            }
+//            .padding(.vertical, 5)
+//        }
+//        .navigationTitle("Comments")
+//        .onAppear {
+//            progress.incrementCounter()
+//            print("task 1")
+//        }
+//    }
+//}
+//
+//struct CommentsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CommentsView(commentUser: .constant("ShadowLurker"), commentText: .constant("That lips look like pongo lips"))
+//            .environmentObject(UserProgress())
+//    }
+//}
 //
 
 import SwiftUI
-
 struct CommentsView: View {
     @EnvironmentObject private var progress: UserProgress
+    @Binding var commentUser: String
+    @Binding var commentText: String
     
-    
-    let comments: [Comment] = [
-        Comment(username: "makeupfanatic", text: "Love this look! Can you share your routine?"),
-        Comment(username: "beautyjunkie", text: "Your makeup is always on point!"),
-        Comment(username: "ShadowLurker", text: "You're nothing but a fake"),
-        Comment(username: "beautylover", text: "Absolutely stunning!"),
-        Comment(username: "glamqueen", text: "You look gorgeous! What's your lipstick shade?"),
-        Comment(username: "nataliebert", text: "So stunning😍"),
-        Comment(username: "stephaniejenn", text: "Love this look on you!❤️"),
-        Comment(username: "rosemisela", text: "Absolutely beautiful!"),
-        Comment(username: "makeupaddict", text: "I need this look in my life!"),
-        Comment(username: "beautybabe", text: "Your makeup is flawless!"),
-        Comment(username: "cosmeticqueen", text: "You're glowing!"),
-        Comment(username: "makeuplover", text: "Stunning as always!😍"),
-        Comment(username: "makeupenthusiast", text: "Can't get over how amazing you look!"),
-        Comment(username: "beautyobsessed", text: "You're my makeup inspiration!"),
-        Comment(username: "glamourfan", text: "Such a beautiful look!"),
-        Comment(username: "makeupgeek", text: "Love this so much!"),
-        Comment(username: "beautyaddicted", text: "Your makeup skills are incredible!"),
-        Comment(username: "cosmeticfanatic", text: "You always look amazing!"),
-        Comment(username: "glamsquad", text: "Love this! You're so talented!"),
-        Comment(username: "beautyicon", text: "Absolutely gorgeous!")
-    ]
-
+    var comments: [Comment] {
+        var tempComments = [
+            Comment(username: "makeupfanatic", text: "Love this look! Can you share your routine?"),
+            Comment(username: "beautyjunkie", text: "Your makeup is always on point!"),
+            Comment(username: "ShadowLurker", text: "You're nothing but a fake"),
+            Comment(username: "beautylover", text: "Absolutely stunning!"),
+            Comment(username: "glamqueen", text: "You look gorgeous! What's your lipstick shade?"),
+            Comment(username: "nataliebert", text: "So stunning😍"),
+            Comment(username: "stephaniejenn", text: "Love this look on you!❤️"),
+            Comment(username: "rosemisela", text: "Absolutely beautiful!"),
+            Comment(username: "makeupaddict", text: "I need this look in my life!"),
+            Comment(username: "beautybabe", text: "Your makeup is flawless!"),
+            Comment(username: "cosmeticqueen", text: "You're glowing!"),
+            Comment(username: "makeuplover", text: "Stunning as always!😍"),
+            Comment(username: "makeupenthusiast", text: "Can't get over how amazing you look!"),
+            Comment(username: "beautyobsessed", text: "You're my makeup inspiration!"),
+            Comment(username: "glamourfan", text: "Such a beautiful look!"),
+            Comment(username: "makeupgeek", text: "Love this so much!"),
+            Comment(username: "beautyaddicted", text: "Your makeup skills are incredible!"),
+            Comment(username: "cosmeticfanatic", text: "You always look amazing!"),
+            Comment(username: "glamsquad", text: "Love this! You're so talented!"),
+            Comment(username: "beautyicon", text: "Absolutely gorgeous!")
+        ]
+        
+        tempComments.insert(Comment(username: commentUser, text: commentText), at: 0)
+        
+        return tempComments
+    }
     
     var body: some View {
         List(comments) { comment in
@@ -57,6 +128,8 @@ struct CommentsView: View {
 
 struct CommentsView_Previews: PreviewProvider {
     static var previews: some View {
-        CommentsView().environmentObject(UserProgress())
+        CommentsView(commentUser: .constant("ShadowLurker"), commentText: .constant("That lips look like pongo lips"))
+            .environmentObject(UserProgress())
     }
 }
+
--- Mini 2/View/PostView.swift
+++ Mini 2/View/PostView.swift
@@ -1,3 +1,152 @@
+
+//import SwiftUI
+//
+//struct PostView: View {
+//    let username: String
+//    let caption: String
+//    let location: String
+//    let postImage: String
+//    let profileImage: String
+//    let initialLikes: Int
+//    
+//    @State private var isLiked = false
+//    @State private var likesCount: Int
+//    @State private var commentUser: String
+//    @State private var commentText: String
+//    
+//    init(username: String, caption: String, location: String, postImage: String, profileImage: String, initialLikes: Int, commentUser: String, commentText: String) {
+//        self.username = username
+//        self.caption = caption
+//        self.location = location
+//        self.postImage = postImage
+//        self.profileImage = profileImage
+//        self.initialLikes = initialLikes
+//        _likesCount = State(initialValue: initialLikes)
+//        _commentUser = State(initialValue: commentUser)
+//        _commentText = State(initialValue: commentText)
+//    }
+//    
+//    var topComment: Comment {
+//        Comment(username: commentUser, text: commentText)
+//    }
+//    
+//    var body: some View {
+//        VStack {
+//            HStack(alignment: .top) {
+//                Image(profileImage)
+//                    .resizable()
+//                    .scaledToFill()
+//                    .frame(width: 50, height: 50)
+//                    .cornerRadius(25)
+//                
+//                VStack(alignment: .leading) {
+//                    Text(username)
+//                        .font(.system(size: 18))
+//                        .foregroundColor(.black)
+//                        .bold()
+//                    
+//                    Text(location)
+//                        .font(.system(size: 15))
+//                        .foregroundColor(.gray)
+//                }
+//                .padding(.top, 3)
+//                
+//                Spacer()
+//                
+//                Button {
+//                    // action for button
+//                } label: {
+//                    Image("dots")
+//                        .resizable()
+//                        .frame(width: 30, height: 30)
+//                        .padding(.trailing, 15)
+//                        .padding(.top, 5)
+//                }
+//            }
+//            .padding(.leading, 15)
+//            .padding(.bottom, 5)
+//            
+//            ZStack(alignment: .topLeading) {
+//                Image(postImage)
+//                    .resizable()
+//                    .scaledToFill()
+//                    .cornerRadius(20)
+//                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                    .padding(.trailing, 15)
+//                    .padding(.leading, 15)
+//            }
+//            
+//            VStack(alignment: .leading, spacing: 3) {
+//                HStack(spacing: 20) {
+//                    Button {
+//                        isLiked.toggle()
+//                        likesCount += isLiked ? 1 : -1
+//                    } label: {
+//                        Image(systemName: isLiked ? "heart.fill" : "heart")
+//                            .resizable()
+//                            .foregroundColor(isLiked ? .red : .black)
+//                            .frame(width: 25, height: 25)
+//                    }
+//                    
+//                    Button {
+//                        print("comment button tapped")
+//                    } label: {
+//                        Image(systemName: "ellipsis.bubble")
+//                            .resizable()
+//                            .foregroundColor(.black)
+//                            .frame(width: 25, height: 25)
+//                    }
+//                }
+//                .frame(maxWidth: .infinity, alignment: .leading)
+//                .padding(.leading, 18)
+//                
+//                Text("\(likesCount) others")
+//                    .font(.system(size: 16))
+//                    .bold()
+//                    .padding(.leading, 18)
+//                    .padding(.top, 10)
+//                
+//                HStack {
+//                    Text(username)
+//                        .font(.system(size: 16))
+//                        .bold()
+//                        .padding(.leading, 18)
+//                        .padding(.top, 5)
+//                    
+//                    Text(caption)
+//                        .font(.system(size: 16))
+//                        .padding(.top, 5)
+//                }
+//                
+//                NavigationLink(destination: CommentsView(commentUser: $commentUser, commentText: $commentText)) {
+//                    Text("View all comments")
+//                        .foregroundColor(.gray)
+//                        .font(.system(size: 16))
+//                        .padding(.leading, 18)
+//                }
+//                .padding(.top, 3)
+//                
+//                HStack {
+//                    Text(topComment.username)
+//                        .foregroundColor(.black)
+//                        .font(.system(size: 16))
+//                        .bold()
+//                        .padding(.leading, 18)
+//                    
+//                    Text(topComment.text)
+//                        .font(.system(size: 16))
+//                        .foregroundColor(.black)
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//struct PostView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PostView(username: "beautyjasmine", caption: "beauty Mascara🫶🏻", location: "Home", postImage: "post1", profileImage: "beautyjasmine", initialLikes: 319872, commentUser: "ShadowLurker", commentText: "That lips look like pongo lips")
+//    }
+//}
 import SwiftUI
 
 struct PostView: View {
@@ -7,12 +156,11 @@ struct PostView: View {
     let postImage: String
     let profileImage: String
     let initialLikes: Int
-    let commentUser: String
-    let commentText: String
     
     @State private var isLiked = false
     @State private var likesCount: Int
-    
+    @State private var commentUser: String
+    @State private var commentText: String
     
     init(username: String, caption: String, location: String, postImage: String, profileImage: String, initialLikes: Int, commentUser: String, commentText: String) {
         self.username = username
@@ -21,9 +169,9 @@ struct PostView: View {
         self.postImage = postImage
         self.profileImage = profileImage
         self.initialLikes = initialLikes
-        self.commentUser = commentUser
-        self.commentText = commentText
         _likesCount = State(initialValue: initialLikes)
+        _commentUser = State(initialValue: commentUser)
+        _commentText = State(initialValue: commentText)
     }
     
     var body: some View {
@@ -32,8 +180,8 @@ struct PostView: View {
                 Image(profileImage)
                     .resizable()
                     .scaledToFill()
-                    .frame(minWidth: 50, maxWidth: 50, minHeight: 50, maxHeight: 50)
-                    .cornerRadius(100)
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(25)
                 
                 VStack(alignment: .leading) {
                     Text(username)
@@ -50,7 +198,7 @@ struct PostView: View {
                 Spacer()
                 
                 Button {
-                    
+                    // action for button
                 } label: {
                     Image("dots")
                         .resizable()
@@ -62,7 +210,7 @@ struct PostView: View {
             .padding(.leading, 15)
             .padding(.bottom, 5)
             
-            ZStack(alignment: .topLeading){
+            ZStack(alignment: .topLeading) {
                 Image(postImage)
                     .resizable()
                     .scaledToFill()
@@ -114,7 +262,7 @@ struct PostView: View {
                         .padding(.top, 5)
                 }
                 
-                NavigationLink(destination: CommentsView()) {
+                NavigationLink(destination: CommentsView(commentUser: $commentUser, commentText: $commentText)) {
                     Text("View all comments")
                         .foregroundColor(.gray)
                         .font(.system(size: 16))
@@ -122,18 +270,16 @@ struct PostView: View {
                 }
                 .padding(.top, 3)
                 
-                NavigationLink(destination: StalkerView()) {
-                    HStack {
-                        Text(commentUser)
-                            .foregroundColor(.black)
-                            .font(.system(size: 16))
-                            .bold()
-                            .padding(.leading, 18)
-                        
-                        Text(commentText)
-                            .font(.system(size: 16))
-                            .foregroundColor(.black)
-                    }
+                HStack {
+                    Text(commentUser)
+                        .foregroundColor(.black)
+                        .font(.system(size: 16))
+                        .bold()
+                        .padding(.leading, 18)
+                    
+                    Text(commentText)
+                        .font(.system(size: 16))
+                        .foregroundColor(.black)
                 }
             }
         }
